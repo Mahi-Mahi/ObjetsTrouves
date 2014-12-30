41292b70cf49790d5ffe651e1167f140d9c63b10
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2014-12-30 14:49:49 +0100

    start

--- app.js
+++ app.js
@@ -1,227 +1,157 @@
 "use strict";
-/* global d3 */
+(function() {
 
-var margin = {
-	top: 0,
-	right: 0,
-	bottom: 0,
-	left: 0
-},
-	padding = 1,
-	width = 960 - margin.left - margin.right,
-	height = 500 - margin.top - margin.bottom;
+	/* Events from UI */
 
-var center = {
-	x: width / 2,
-	y: height / 2
-};
+	var region = document.getElementById('region');
+	var picker = document.getElementById('dataCenterPicker');
+	var dataCenter = picker.options[picker.selectedIndex].value;
 
-var svg = d3.select("body").append("svg")
-	.attr("width", width)
-	.attr("height", height);
+	picker.addEventListener('change', function(e) {
+		dataCenter = picker.options[picker.selectedIndex].value;
+		console.log(dataCenter);
+	}, false);
 
-var data, bind;
-var nodes = [];
-var circle;
+	var isRunning = true;
+	var button = document.getElementById('toggle');
 
-var damper = 0.01;
-var collide, gravity, force, radius;
-
-var fill_color = d3.scale.category20();
-
-var force;
-
-function start(nodes) {
-	console.log(nodes);
-	force = d3.layout.force()
-		.nodes(nodes)
-		.size([width, height]);
-}
-
-function draw(nodes) {
-
-	force
-		.gravity(gravity)
-		.charge(charge)
-	// .friction(0.9)
-	.on("tick",
-		function(e) {
-			bind.each(move_towards_center(e.alpha))
-				.attr("cx", function(d) {
-					return d.x;
-				})
-				.attr("cy", function(d) {
-					return d.y;
-				});
-		}
-		/*
-		function(e) {
-		circle.each(gravity(2 * e.alpha))
-			.each(collide(0.1))
-			.attr("cx", function(d) {
-				return d.x;
-			})
-			.attr("cy", function(d) {
-				return d.y;
+	button.addEventListener('click', function(e) {
+		if (isRunning) {
+			pubnub.unsubscribe({
+				channel: channel
 			});
-	}
-*/
-	)
-		.on('end', function() {
-			console.log("TICKED");
-		});
-	force.start();
-
-}
-
-function move_towards_center(alpha) {
-	return function(d) {
-		d.x = d.x + (center.x - d.x) * (damper + 0.02) * alpha;
-		d.y = d.y + (center.y - d.y) * (damper + 0.02) * alpha;
-	};
-}
-
-function gravity(alpha) {
-	return function(d) {
-		d.y += (d.cy - d.y) * alpha;
-		d.x += (d.cx - d.x) * alpha;
-	};
-}
-
-// Resolve collisions between nodes.
-
-function collide(alpha) {
-	var quadtree = d3.geom.quadtree(nodes);
-	return function(d) {
-		var r = d.radius + radius.domain()[1] + padding,
-			nx1 = d.x - r,
-			nx2 = d.x + r,
-			ny1 = d.y - r,
-			ny2 = d.y + r;
-		quadtree.visit(function(quad, x1, y1, x2, y2) {
-			if (quad.point && (quad.point !== d)) {
-				var x = d.x - quad.point.x,
-					y = d.y - quad.point.y,
-					l = Math.sqrt(x * x + y * y),
-					r = d.radius + quad.point.radius + (d.color !== quad.point.color) * padding;
-				if (l < r) {
-					l = (l - r) / l * alpha;
-					d.x -= x *= l;
-					d.y -= y *= l;
-					quad.point.x += x;
-					quad.point.y += y;
-				}
-			}
-			return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-		});
-	};
-}
-
-function charge(d, i) {
-	// return 0;
-	return -Math.pow(d.radius, 2.0) / 25;
-}
-
-function sanitize(s) {
-	return s.replace(/[^\w]/g, '-');
-}
-
-d3.csv("genres.csv", function(error, source) {
-
-	data = source;
-
-	var max = d3.max(source, function(d) {
-		d.deposes = parseInt(d.deposes_i, 10) + parseInt(d.deposes_ni, 10);
-		return parseInt(d.deposes, 10);
-	});
-
-	radius = d3.scale.sqrt().domain([0, max]).range([0, 95]);
-	// radius = d3.scale.sqrt().range([0, 12]);
-
-	source.forEach(function(d) {
-		var node = {
-			id: sanitize(d.genre),
-			original: d,
-			radius: radius(parseInt(d.deposes, 10)),
-			cx: Math.random() * width,
-			cy: Math.random() * height
-		};
-		nodes.push(node);
-	});
-
-	console.log(nodes);
-
-	bind = svg.selectAll("circle")
-		.data(nodes);
-
-	circle = bind
-		.enter()
-		.append("circle")
-		.attr('id', function(d) {
-			return 'circle-' + d.id;
-		})
-		.attr("r", function(d) {
-			return d.radius;
-		})
-		.style("fill", function(d) {
-			return fill_color(d.id);
-		});
-
-	// Move nodes toward cluster focus.
-	start(nodes);
-	draw();
-});
-
-jQuery(function() {
-	jQuery('#filters li').on('click', function() {
-
-		var match = jQuery(this).text();
-		if (match == 'Tout') {
-			match = null;
+			button.value = 'Stream again';
+			isRunning = false;
 		} else {
-			match = new RegExp(match, "i");
+			getData();
+			button.value = 'Stop me!';
+			isRunning = true;
 		}
 
-		var filtered = [];
-		data.forEach(function(d) {
-			filtered.push(d);
-		});
+	}, false);
 
-		var max = d3.max(filtered, function(d) {
-			d.deposes = parseInt(d.deposes_i, 10) + parseInt(d.deposes_ni, 10);
-			return (!match || d.genre.match(match)) ? parseInt(d.deposes, 10) : 0;
-		});
+	/* D3 Bubble Chart */
 
-		console.log(jQuery(this).text(), max);
+	var diameter = Math.min(document.getElementById('chart').clientWidth, window.innerHeight - document.querySelector('header').clientHeight) - 20;
 
-		var radius = d3.scale.sqrt().domain([0, max]).range([0, 95]);
+	var svg = d3.select('#chart').append('svg')
+		.attr('width', diameter)
+		.attr('height', diameter);
 
-		nodes = [];
-		filtered.forEach(function(d) {
-			var dd = d3.select('#circle-' + sanitize(d.genre));
-			var node = {
-				id: sanitize(d.genre),
-				original: d,
-				radius: (!match || d.genre.match(match)) ? radius(parseInt(d.deposes, 10)) : 0,
-				cx: dd.attr('cx'),
-				cy: dd.attr('cy'),
-			};
-			if (node.radius) {
-				console.log(node.id, node.radius, node.cx, node.cy);
-			}
-			nodes.push(node);
-		});
+	var bubble = d3.layout.pack()
+		.size([diameter, diameter])
+		.value(function(d) {
+			return d.size;
+		}) // new data is loaded to bubble layout
+	.padding(3);
 
-		bind = svg.selectAll("circle")
-			.data(nodes);
+	function drawBubbles(m) {
 
-		bind.transition()
-			.duration(500)
-			.attr('r', function(d) {
-				return d.radius;
+		if (m.region !== dataCenter) return;
+
+		region.textContent = m.region;
+
+		// generate data with calculated layout values
+		var nodes = bubble.nodes(processData(m))
+			.filter(function(d) {
+				return !d.children;
+			}); // filter out the outer bubble
+
+		// assign new data to existing DOM
+		var vis = svg.selectAll('circle')
+			.data(nodes, function(d) {
+				return d.name;
 			});
 
-		// start(nodes);
-		// draw();
+		// enter data -> remove, so non-exist selections for upcoming data won't stay -> enter new data -> ...
+
+		// To chain transitions,
+		// create the transition on the updating elements before the entering elements
+		// because enter.append merges entering elements into the update selection
+
+		var duration = 200;
+		var delay = 0;
+
+		// update - this is created before enter.append. it only applies to updating nodes.
+		vis.transition()
+			.duration(duration)
+			.delay(function(d, i) {
+				delay = i * 7;
+				return delay;
+			})
+			.attr('transform', function(d) {
+				return 'translate(' + d.x + ',' + d.y + ')';
+			})
+			.attr('r', function(d) {
+				return d.r;
+			})
+			.style('opacity', 1); // force to 1, so they don't get stuck below 1 at enter()
+
+		// enter - only applies to incoming elements (once emptying data)
+		vis.enter().append('circle')
+			.attr('transform', function(d) {
+				return 'translate(' + d.x + ',' + d.y + ')';
+			})
+			.attr('r', function(d) {
+				return d.r;
+			})
+			.attr('class', function(d) {
+				return d.className;
+			})
+			.style('opacity', 0)
+			.transition()
+			.duration(duration * 1.2)
+			.style('opacity', 1);
+
+		// exit
+		vis.exit()
+			.transition()
+			.duration(duration + delay)
+			.style('opacity', 0)
+			.remove();
+	}
+
+	/* PubNub */
+
+	var channel = 'rts-xNjiKP4Bg4jgElhhn9v9';
+
+	var pubnub = PUBNUB.init({
+		subscribe_key: 'e19f2bb0-623a-11df-98a1-fbd39d75aa3f'
 	});
-});
+
+	function getData() {
+		var i = 0;
+		pubnub.subscribe({
+			channel: channel,
+			callback: function(m) {
+				// i++;
+				// if(i === 1 || i%10 === 0) {
+				// 	drawBubbles(m);
+				// }
+				drawBubbles(m);
+			}
+		});
+	}
+
+	function processData(data) {
+		if (!data) return;
+
+		var obj = data.countries_msg_vol;
+
+		var newDataSet = [];
+
+		for (var prop in obj) {
+			newDataSet.push({
+				name: prop,
+				className: prop.toLowerCase().replace(/ /g, ''),
+				size: obj[prop]
+			});
+		}
+		return {
+			children: newDataSet
+		};
+	}
+
+	getData();
+
+})();
